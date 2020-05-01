ab88269b9bff65582cf22c6003151cdf9f32e912
John Leavell <johnleavell@gmail.com>
2020-05-01 13:54:58 -0500

    updated users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,22 @@
 class UsersController < ApplicationController
-  def index
-    users = User.all
-    render json: users
-  end
+  skip_before_action :authorized, only: [:create]
 
-  def show
-    user = User.find_by(id: params[:id])
-    
-    if user
-      render json: user
-    else
-      render json: {error: 'User not Found'}
-    end
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
   end
-
-  
 
   def create
-
+    @user = User.create(user_params)
+      if @user.valid?
+        @token = encode_token(user_id: @user.id)
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      else
+        render json: { error: 'failed to create user' }, status: :not_acceptable
+      end
   end
 
-  def update
-  end
-
-  def destroy
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :notes)
   end
 end
